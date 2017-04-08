aa46caacf3fde3e7492fa93b33d536854e03c9be
matias mirotta <m.mirotta@gmail.com>
2017-04-08 10:39:51 -0300

    Listado y creación de tareas en firebase. Utilizando AngularFire2

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -1,38 +1,65 @@
 import { Component } from '@angular/core';
-
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase  } from 'angularfire2';
 
 @Component({
   selector: 'page-page2',
   templateUrl: 'page2.html'
 })
+
 export class Page2 {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  tareas: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public database: AngularFireDatabase
+  ) {
+    this.tareas = this.database.list('/tareas')
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page2, {
-      item: item
+  crearTarea(){
+    let nuevaTareaModal = this.alertCtrl.create({
+      title: 'Nueva Tarea',
+      message: "Ingrese el título de la tarea",
+      inputs: [
+        {
+          name: 'titulo',
+          placeholder: 'Título'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancelar click');
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: data => {
+            this.tareas.push({
+              titulo: data.titulo,
+              activo: false
+            });
+          }
+        }
+      ]
     });
+    nuevaTareaModal.present( nuevaTareaModal );
   }
+
+  modificarTarea( tarea ){
+    setTimeout(()=>{
+      this.tareas.update( tarea.$key,{
+        titulo: tarea.titulo,
+        activo: tarea.activo
+      });
+    },1);
+  }
+
+  eliminarTarea( tarea ){
+    this.tareas.remove( tarea );
+  }
+
 }
